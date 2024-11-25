6b9f0f24e4709fe210ba45cd3bf52db1cea38bd4
Michael-Andryeer <andryeerm@gmail.com>
2024-11-25 17:15:40 -0300

    Criando objeto de usuário

--- frontend/src/components/pages/Auth/Register.jsx
+++ frontend/src/components/pages/Auth/Register.jsx
@@ -10,7 +10,7 @@ export default function Register() {
 
   function handleSubmit(e) {
     e.preventDefault()
-    // Aqui você pode adicionar a lógica de registro
+    // Enviar usuario para o banco de dados
     console.log(user)
   }
 
