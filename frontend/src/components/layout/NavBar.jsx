4a7ea703358e349f57f35c79354f140cb7e64d8f
Michael-Andryeer <andryeerm@gmail.com>
2024-11-26 02:57:19 -0300

    Persistindo usuário autenticado no front

--- frontend/src/components/layout/NavBar.jsx
+++ frontend/src/components/layout/NavBar.jsx
@@ -1,30 +1,46 @@
-import React from "react";
 import { Link } from "react-router-dom";
-import {Button} from '../../components/ui/button'
+import { Button } from '../../components/ui/button';
 import { PawPrint } from 'lucide-react';
 
+// Context
+import { Context } from '../../context/UserContext';
+import { useContext } from "react";
+// Context
+
 export default function Navbar() {
+  const { authenticated } = useContext(Context);
+
   return (
     <nav className="border-b">
       <div className="flex h-16 items-center px-4 max-w-7xl mx-auto">
+        {/* Logo */}
         <Link to="/" className="flex items-center gap-2 font-semibold">
           <PawPrint className="h-6 w-6" />
           <span>PetCadu</span>
         </Link>
-        
+
+        {/* Navegação */}
         <div className="ml-auto flex items-center gap-4">
           <Link to="/">
             <Button variant="ghost">Adotar</Button>
           </Link>
-          <Link to="/login">
-            <Button variant="ghost">Entrar</Button>
-          </Link>
-          <Link to="/register">
-            <Button variant="default">Cadastrar</Button>
-          </Link>
+          
+          {authenticated ? (
+            // Exibe quando o usuário está autenticado
+            <p>Logado</p>
+          ) : (
+            // Exibe quando o usuário NÃO está autenticado
+            <>
+              <Link to="/login">
+                <Button variant="ghost">Entrar</Button>
+              </Link>
+              <Link to="/register">
+                <Button variant="default">Cadastrar</Button>
+              </Link>
+            </>
+          )}
         </div>
       </div>
     </nav>
-  )
+  );
 }
-
